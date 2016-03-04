bdd814bf2d06c5f33f945711ee2646450b25f832
chettriashish <ashish.chettri@nexusdatasystems.co.in>
2016-03-04 01:27:42 -0500

    admin module almost complete

---
+++ MMC.Professional/MMC.Professional/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MMC.Professional.Startup))]
+namespace MMC.Professional
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ MMC.Web/Controllers/MVC/Admin/Bookings/AdminBookingsController.cs
@@ -0,0 +1,52 @@
+﻿using MMC.Client.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MMC.Web.Controllers.Admin.Bookings
+{
+    public class AdminBookingsController : Controller
+    {
+        IActivitiesService _activitiesService;
+        public AdminBookingsController(IActivitiesService activitiesService)
+        {
+            _activitiesService = activitiesService;
+        }
+
+        public ActionResult GetAllActivitiesPendingForConfirmation()
+        {
+            return Json(_activitiesService.GetAllActivitiesPendingForConfirmation(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetAllRegisteredCompanies()
+        {
+            return Json(_activitiesService.GetAllRegisteredCompanies(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetAllActivitiesPendingForSelectedCompany(string companyKey)
+        {
+            return Json(_activitiesService.GetAllCompanyActivitiesPendingForConfirmation(companyKey), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetAllCompletedActivities()
+        {
+            return Json(_activitiesService.GetAllActivitiesCompleted(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetAllActivitiesCompletedForSelectedCompany(string companyKey)
+        {
+            return Json(_activitiesService.GetAllCompanyActivitiesCompleted(companyKey), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult GetAllUpcomingActivities()
+        {
+            return Json(_activitiesService.GetAllUpcomingActivities(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetAllUpcomingCompanyActivities(string companyKey)
+        {
+            return Json(_activitiesService.GetAllUpcomingCompanyActivities(companyKey), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
